d7eb47975fcf90b6a547c96e9e45ce22539ab4ac
Carlos Henrique <carloshenriquepvh@hotmail.com>
2022-07-21 18:22:56 -0400

    Implementação do horario e outras melhorias em JS

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,41 @@ import { Sidebar } from "./components/Sidebar";
 import styles from './components/App.module.css';
 import './global.css';
 
+const posts = [
+  {
+      id: 1,
+      author: {
+          avatarUrl:"https://github.com/JRSparrowII.png",
+          name: 'Carlos Henrique',
+          role: "Web Developer",
+      },
+      content: [
+          { type: 'paragraph', content: 'Ola pessoal',},
+          { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu Github',},
+          // { type: 'link', content: 'nemesio.veloso/designer',},   
+         
+      ],
+      publishedAt:new Date('2022-07-07 20:00:00'),
+
+  },
+  {
+      id: 2,
+      author: {
+          avatarUrl:"https://github.com/priscocleyton.png",
+          name: 'Prisco Cleyton',
+          role: "Mobile Developer",
+      },
+      content: [
+          { type: 'paragraph', content: 'Ola pessoal',},
+          { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu Github',},
+          // { type: 'link', content: 'nemesio.veloso/designer',},   
+         
+      ],
+      publishedAt:new Date('2022-07-09 20:00:00'),
+      
+  }
+]
+
 
 export function App() {  
   return (
@@ -13,14 +48,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post
-            author="Henrique"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quas nesciunt laboriosam adipisci illo reiciendis dolor eum, cupiditate placeat, quisquam, quos nulla suscipit veniam iusto! Debitis nulla id placeat facilis."
-          />
-          <Post
-            author="Prisco"
-            content="To fazendo um projeto bem legal."
-          />
+        {posts.map(post => {
+          return (
+            <Post          
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+        })}
         </main>        
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,26 +2,33 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post(){
+
+export function Post({author, publishedAt}){
+    const publishedDateFormated = new Intl.DateTimeFormat('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        hour: '2-digit',
+        minute: '2-digit'
+        // timeStyle: 'full',
+
+    }).format(publishedAt);
+
  return(
     <article className={styles.post}>
         <header>
             <div className={styles.author}>
-                <Avatar src= "https://github.com/netovelosonog.png"/>
+                <Avatar src={author.avatarUrl}/>
                 <div className={styles.authorInfo}>
-                    <strong>Nemesio Veloso</strong>
-                    <span>Web Developer</span>
+                    <strong>{author.name}</strong>
+                    <span>{author.role}</span>
                 </div>
             </div>
             <time title="19 de julho de 2022 as 18:45h" dateTime='2022-07-19 08:11:37'>
-                Publicado há 1h
+                {publishedDateFormated}
             </time>
         </header>
         <div className={styles.content}>
-            <p>Ola pessoal</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum reprehenderit et eos adipisci officiis modi quod iure incidunt, quaerat veniam illum assumenda, iusto aspernatur ducimus aliquam voluptates, sed voluptatibus numquam.</p>
-            <p> <a href="#"> henrique.designer.com</a></p>
-            <p> <a href="http://"> #Novoprojeto #Rockseat</a></p>
+            
         </div>
         <form className={styles.commentForm}>
             <strong>Deixe seu feedback</strong>
